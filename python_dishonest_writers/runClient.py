3a5c44b80e8e1e17dcd15b06c7a16dfd5effc0d8
Cayke Prudente <cayke10@gmail.com>
2017-04-20 16:29:43 -0300

    - cliente teste python

--- python_dishonest_writers/runClient.py
+++ python_dishonest_writers/runClient.py
@@ -1,4 +1,9 @@
 from client import Client
+from testClient import TestClient
 
-servers = [('localhost', 5000),('localhost', 5001),('localhost', 5002), ('localhost', 5003)]
-Client(0, servers)
+# servers = [('localhost', 5000),('localhost', 5001),('localhost', 5002), ('localhost', 5003)]
+# Client(0, servers)
+
+
+servers = [('localhost', 5000),('localhost', 5001)]
+TestClient(0 , servers)
