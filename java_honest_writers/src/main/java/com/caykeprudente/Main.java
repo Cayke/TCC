469317aa8f59f0f27f59481b9fb915496278765c
Cayke Prudente <cayke10@gmail.com>
2017-07-20 19:50:31 -0300

    - honest servers running on linux

--- java_honest_writers/src/main/java/com/caykeprudente/Main.java
+++ java_honest_writers/src/main/java/com/caykeprudente/Main.java
@@ -10,17 +10,17 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        //runClient();
-        runServer(args);
+        runClient();
+        //runServer(args);
     }
 
     private static void runServer(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Numero de argumentos invalidos");
+        if (args.length < 1) {
+            System.out.println(String.format("Numero de argumentos invalidos: %d", args.length));
             return;
         }
 
-        Server server = new Server(0, args[1], 5000);
+        Server server = new Server(0, args[0], 5000);
         try {
             server.waitForConnection();
         } catch (IOException e) {
@@ -30,9 +30,10 @@ public class Main {
 
     private static void runClient() {
         List<Pair<String, Integer>> servers = new ArrayList<Pair<String, Integer>>();
-        servers.add(new Pair<String, Integer>("localhost", 5000));
-        servers.add(new Pair<String, Integer>("localhost", 5001));
-        servers.add(new Pair<String, Integer>("localhost", 5002));
+        servers.add(new Pair<String, Integer>("node0.caykequoruns.freestore.emulab.net", 5000));
+        servers.add(new Pair<String, Integer>("node1.caykequoruns.freestore.emulab.net", 5000));
+        servers.add(new Pair<String, Integer>("node2.caykequoruns.freestore.emulab.net", 5000));
+        servers.add(new Pair<String, Integer>("node3.caykequoruns.freestore.emulab.net", 5000));
         new Client(0d, servers);
     }
 }
--- python/runClient.py
+++ python/runClient.py
@@ -1,4 +1,4 @@
 from client import Client
 
-servers = [('localhost', 5000),('localhost', 5001),('localhost', 5002)]
+servers = [('node0.caykequoruns.freestore.emulab.net', 5000),('node2.caykequoruns.freestore.emulab.net', 5000),('node4.caykequoruns.freestore.emulab.net', 5000)]
 Client(0, servers)
