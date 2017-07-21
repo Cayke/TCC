fb77e3868bcdeb7476c2d80693569d2101e364bf
Cayke Prudente <cayke10@gmail.com>
2017-07-21 19:48:40 -0300

    - java dishonest writers argv

--- java_dishonest_writers/src/main/java/com/caykeprudente/Main.java
+++ java_dishonest_writers/src/main/java/com/caykeprudente/Main.java
@@ -9,27 +9,31 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
-        runServer(3d);
-        //runClient();
+        // write your code here
+        runClient();
+        //runServer(args);
     }
 
-    private static void runServer(Double id) {
-        Server server = new Server(id, "localhost", 5000 + id.intValue());
+    private static void runServer(String[] args) {
+        if (args.length < 1) {
+            System.out.println(String.format("Numero de argumentos invalidos: %d", args.length));
+            return;
+        }
+
+        Server server = new Server(0d, args[0], 5000);
         try {
             server.waitForConnection();
         } catch (IOException e) {
-            e.printStackTrace();
             System.out.println("Deu ruim na main");
         }
     }
 
     private static void runClient() {
         List<Pair<String, Integer>> servers = new ArrayList<Pair<String, Integer>>();
-        servers.add(new Pair<String, Integer>("localhost", 5000));
-        servers.add(new Pair<String, Integer>("localhost", 5001));
-        servers.add(new Pair<String, Integer>("localhost", 5002));
-        servers.add(new Pair<String, Integer>("localhost", 5003));
+        servers.add(new Pair<String, Integer>("node0.caykequoruns.freestore.emulab.net", 5000));
+        servers.add(new Pair<String, Integer>("node1.caykequoruns.freestore.emulab.net", 5000));
+        servers.add(new Pair<String, Integer>("node2.caykequoruns.freestore.emulab.net", 5000));
+        servers.add(new Pair<String, Integer>("node3.caykequoruns.freestore.emulab.net", 5000));
         new Client(servers);
     }
 }
